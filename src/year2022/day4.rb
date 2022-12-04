fcf065e7298ba278ca00c7b86d304a6bfb8d6a2c
iMneztra <git@martzen.nl>
2022-12-04 19:40:29 +0100

    refactored solution day4

--- src/year2022/day4.rb
+++ src/year2022/day4.rb
@@ -6,15 +6,11 @@ overlap = 0
 File.read('input/day4.txt').lines do |line|
     pairs = line.chomp.split(",")
 
-    e1 = pairs[0].split("-")
-    e2 = pairs[1].split("-")
-    e1s = e1[0].to_i
-    e1e = e1[1].to_i
-    e2s = e2[0].to_i
-    e2e = e2[1].to_i
-
-    e1s <= e2s && e2e <= e1e || e2s <= e1s && e1e <= e2e ? full_match +=1 : full_match +=0
-    e1s <= e2e && e2s <= e1e ? overlap +=1 : overlap +=0
+    e1 = pairs[0].split("-").map(&:to_i)
+    e2 = pairs[1].split("-").map(&:to_i)
+    
+    e1.first <= e2.first && e2.last <= e1.last || e2.first <= e1.first && e1.last <= e2.last ? full_match +=1 : full_match +=0
+    e1.first <= e2.last && e2.first <= e1.last ? overlap +=1 : overlap +=0
 end
 
 # Puzzle 1 
